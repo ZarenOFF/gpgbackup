e11b96e9a0fc488b3811414f64d5b8bbcde807c8
ZarenOFF <f.o.b.o@yandex.ru>
2024-07-05 11:48:34 +0300

    Added -n flag to set backup name

--- gpgbackup.sh
+++ gpgbackup.sh
@@ -9,17 +9,20 @@ WEBDAV_USER="your_webdav_username"  # Имя пользователя WebDAV
 WEBDAV_PASS_FILE="/root/.webdavpass"  # Файл с паролем WebDAV
 GPG_RECIPIENT="put_your_recipient"  # Получатель GPG (email или ключ)
 
+
 usage() {
   echo "Usage: $0 -o <directory_or_file_to_backup> [-o <another_directory_or_file_to_backup> ...] -r <gpg_recipient>"
   exit 1
 }
 
 BACKUP_SOURCES=()
+BACKUP_NAME=""
 
-while getopts "o:r:" opt; do
+while getopts "o:r:n:" opt; do
   case $opt in
     o) BACKUP_SOURCES+=("$OPTARG") ;;
     r) GPG_RECIPIENT="$OPTARG" ;;
+	  n) BACKUP_NAME="$OPTARG" ;;
     *) usage ;;
   esac
 done
@@ -29,9 +32,17 @@ if [ ${#BACKUP_SOURCES[@]} -eq 0 ]; then
   usage
 fi
 
+# Создание имени архива
+if [ -z "$BACKUP_NAME" ];
+then
+  TIMESTAMP=$(date +"%d-%m-%Y_%H-%M-%S")
+  ARCHIVE_NAME="backup_$TIMESTAMP.tar.gz"
+else
+  TIMESTAMP=$(date +"%d-%m-%Y_%H-%M-%S")
+  ARCHIVE_NAME="${BACKUP_NAME}_$TIMESTAMP.tar.gz"
+fi
+
 # Создание архива для каждого источника
-TIMESTAMP=$(date +"%d-%m-%Y_%H-%M-%S")
-ARCHIVE_NAME="backup_$TIMESTAMP.tar.gz"
 tar -czf "$BACKUP_DEST/$ARCHIVE_NAME" "${BACKUP_SOURCES[@]}"
 
 # Шифрование архива
