006ed7b179c445a6c810d51e0e0e9822f4ea7ea7
ZarenOFF <f.o.b.o@yandex.ru>
2024-07-09 11:29:57 +0300

    Added config support and checking required vars

--- gpgbackup.sh
+++ gpgbackup.sh
@@ -1,36 +1,64 @@
 #!/bin/bash
 
-# Usage - ./gpgbackup.sh -o /path/to/dir1 -o /path/to/file2 -o /path/to/dir3 -r recipient@example.com
-
-# Параметры
-BACKUP_DEST="/backup/systembackup/"  # Путь для временного хранения бэкапа на сервере
-GPG_RECIPIENT="put_your_recipient"  # Получатель GPG (email или ключ)
-RCLONE_ID="rcloneid"
-
+# Usage - ./gpgbackup.sh -o /path/to/dir1 -o /path/to/file2 -o /path/to/dir3 -r recipient@example.com -n my_backup -p /path/to/remotefolder
+# Usage with config - ./gpgbackup.sh -c /path/to/backup.conf -o /path/to/dir1 -o /path/to/file2 -o /path/to/dir3
 
 usage() {
   echo "Usage: $0 -o <directory_or_file_to_backup> [-o <another_directory_or_file_to_backup> ...] -r <gpg_recipient>"
   exit 1
 }
 
-BACKUP_SOURCES=()
-REMOTE_PATH="/path_to_your_backups/"
-BACKUP_NAME=""
+# Параметры
+BACKUP_DEST=""  # Путь для временного хранения бэкапа на сервере, без ключа
+GPG_RECIPIENT=""  # Получатель GPG (email или ключ), ключ -r
+RCLONE_ID="" # Rclone ID для копирования
+BACKUP_SOURCES=() # Источники бэкапа
+REMOTE_PATH="" # Путь для временных архивов
+BACKUP_NAME=""  # Имя архива
 
-while getopts "o:r:n:p:" opt; do
-  case $opt in
-    o) BACKUP_SOURCES+=("$OPTARG") ;;
-    r) GPG_RECIPIENT="$OPTARG" ;;
-	  n) BACKUP_NAME="$OPTARG" ;;
-	  p) REMOTE_PATH="$OPTARG" ;;
-    *) usage ;;
-  esac
-done
+# Функция для обработки аргументов командной строки
+process_args() {
+  local OPTIND
+  while getopts "c:o:r:n:p:" opt; do
+    case $opt in
+      c) CONFIG_FILE="$OPTARG" ;;
+      o) BACKUP_SOURCES+=("$OPTARG") ;;
+      r) GPG_RECIPIENT="$OPTARG" ;;
+      n) BACKUP_NAME="$OPTARG" ;;
+      p) REMOTE_PATH="$OPTARG" ;;
+      *) usage ;;
+    esac
+  done
+}
 
-# Проверка наличия обязательных аргументов
-if [ ${#BACKUP_SOURCES[@]} -eq 0 ]; then
-  usage
+# Функция для проверки наличия обязательных переменных
+check_required_vars() {
+  if [ -z "$BACKUP_DEST" ] || [ -z "$GPG_RECIPIENT" ] || [ -z "$RCLONE_ID" ] || [ ${#BACKUP_SOURCES[@]} -eq 0 ] || [ -z "$REMOTE_PATH" ]; then
+    echo "Missing required parameters. Please provide either a config file or all necessary parameters."
+    #usage
+  fi
+}
+
+# Первичное чтение параметров командной строки
+process_args "$@"
+
+# Проверка наличия файла конфигурации, если существует, то загружаем переменные
+if [ -n "$CONFIG_FILE" ]; then
+  if [ -f "$CONFIG_FILE" ]; then
+    source "$CONFIG_FILE"
+    #IFS=' ' read -r -a BACKUP_SOURCES <<< "$BACKUP_SOURCES"
+  else
+    echo "Config file not found!"
+    exit 1
+  fi
 fi
+
+# Повторное чтение параметров командной строки для перезаписи значений из конфигурационного файла
+OPTIND=1
+process_args "$@"
+
+# Проверка наличия обязательных переменных
+check_required_vars
 
 # Создание имени архива
 if [ -z "$BACKUP_NAME" ];
@@ -55,4 +83,4 @@ rm "$BACKUP_DEST/$ARCHIVE_NAME"
 rclone copy "$BACKUP_DEST/$ENCRYPTED_FILENAME" ${RCLONE_ID}:${REMOTE_PATH}
 
 # Очистка временных файлов
-#rm "$BACKUP_DEST/$ENCRYPTED_FILENAME"
+rm "$BACKUP_DEST/$ENCRYPTED_FILENAME"
